eae6d981f5c095e05fd68e0cc4e26af092e45039
Alex Glancevs <alex2531458@gmail.com>
2025-01-09 17:54:02 +0200

    feat: add navigation bar component structure

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,24 @@
+import { faMugHot } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { NavLink } from "react-router-dom";
+
 const NavBar = () => {
     return (
-        <nav className="nav"></nav>
+        <nav className="nav">
+            <FontAwesomeIcon icon={faMugHot} className="nav__logo" />
+            <ul className="nav__list" role="list">
+                <li className="nav__link">
+                    <NavLink to='/' className={ ({isActive}) => isActive ? 'active-link' : undefined }>
+                        Home
+                    </NavLink>
+                </li>
+                <li className="nav__link">
+                    <NavLink to='/about' className={ ({isActive}) => isActive ? 'active-link' : undefined }>
+                        About
+                    </NavLink>
+                </li>
+            </ul>
+        </nav>
     )
 }
 
