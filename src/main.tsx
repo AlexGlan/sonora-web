fd9a61e7c2b51eb67b212c296f9869c464b992d1
Alex Glancevs <alex2531458@gmail.com>
2025-01-09 16:15:54 +0200

    feat: create and implement redux store

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import routesConfig from './routes/routesConfig';
+import { setupStore } from './store/store';
+import { Provider } from 'react-redux';
 import './styles/main.scss';
 
 const router = createBrowserRouter(routesConfig, {basename: '/white-noise-app/'});
+const store = setupStore();
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <RouterProvider router={router} />
+        <Provider store={store}>
+            <RouterProvider router={router} />
+        </Provider>
     </StrictMode>,
 );
