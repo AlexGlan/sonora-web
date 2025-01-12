6e84f1cc3bee36086c56126cd983c65c95faa957
Alex Glancevs <alex2531458@gmail.com>
2025-01-12 23:02:27 +0200

    feat: enhance audio tracks with icons and styled slider (#8)

--- src/components/AudioTrack.tsx
+++ src/components/AudioTrack.tsx
@@ -3,6 +3,7 @@ import { faPause, faPlay } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useAppDispatch, useAppSelector } from "../app/hooks";
 import { setVolume, setPlayStatus } from "../store/audioSlice";
+import { getImageUrl } from "../utils/imageUtils";
 
 type AudioTrackProps = {
     trackId: number,
@@ -48,6 +49,14 @@ const AudioTrack = ({ trackId }: AudioTrackProps) => {
 
     return (
         <div className="track">
+            <img
+                src={getImageUrl(track.iconName + '-solid-icon.svg')}
+                alt={`${track.name} icon`}
+                className="track__icon"
+                style={{
+                    filter: `invert(0.65) sepia(1) saturate(1.8) hue-rotate(${track.isPlaying ? '120':'200'}deg)`
+                }}
+            />
             <span className="track__name">{track.name}</span>
             <div className="track__controls">
                 <button
@@ -69,6 +78,9 @@ const AudioTrack = ({ trackId }: AudioTrackProps) => {
                     onChange={(e) => handleVolumeChange(e.target.value)}
                     className="track__volume-slider"
                     aria-label="Volume slider"
+                    style={{
+                        background: `linear-gradient(to right, #cba6f7 ${track.volume}%, #45475a ${track.volume}%)`
+                    }}
                 />
             </div>
             <audio
