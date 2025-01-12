678ef38468d23fdfe95ce7e0042e60823502e33d
Alex Glancevs <alex2531458@gmail.com>
2025-01-12 23:53:28 +0200

    feat: add error page to home route

--- src/routes/routesConfig.tsx
+++ src/routes/routesConfig.tsx
@@ -1,10 +1,13 @@
 import Home from "../pages/Home";
+import PageNotFound from "../pages/PageNotFound";
 
 const routesConfig = [
     {
         path: '/',
         element: <Home />,
+        errorElement: <PageNotFound />
     },
+
 ];
 
 export default routesConfig;
