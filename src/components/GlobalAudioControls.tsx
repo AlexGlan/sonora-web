f0e24adb264988576931b6605ce4c1a7d2eeecd6
Alex Glancevs <alex2531458@gmail.com>
2025-01-13 21:37:09 +0200

    feat: create global audio controls component

---
+++ src/components/GlobalAudioControls.tsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import { useAppDispatch } from "../app/hooks";
+import { resetTracks, setGlobalPlayStatus, setGlobalVolume } from "../store/audioSlice";
+import { faArrowRotateRight, faPause, faPlay } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const GlobalAudioControls = () => {
+    const [isPlaying, setIsPlaying] = useState<boolean>(false);
+    const [volume, setVolume] = useState<number>(30);
+    const dispatch = useAppDispatch();
+
+    const handleReset = (): void => {
+        dispatch(resetTracks());
+        setIsPlaying(false);
+        setVolume(30);
+    }
+
+    const handleGlobalPlayback = (): void => {
+        dispatch(setGlobalPlayStatus(!isPlaying));
+        setIsPlaying(prevVal => {
+            return !prevVal;
+        });
+    }
+    const handleGlobalVolumeChange = (volumeVal: string): void => {
+        dispatch(setGlobalVolume(volumeVal));
+        setVolume(parseInt(volumeVal));
+    }
+
+    return (
+        <div className="global-controls">
+            <div className="global-controls__button-container">
+                <button
+                    onClick={handleReset}
+                    className="global-controls__btn"
+                    aria-label="Reset tracks"
+                >
+                    <FontAwesomeIcon icon={faArrowRotateRight} className="global-controls__icon" />
+                </button>
+                <button
+                    onClick={handleGlobalPlayback}
+                    className="global-controls__btn"
+                    aria-label={isPlaying ? 'Pause' : 'Play'}
+                    >
+                        <FontAwesomeIcon icon={isPlaying ? faPause : faPlay} className="global-controls__icon" />
+                </button>
+            </div>
+            <input
+                type="range"
+                min={0}
+                max={100}
+                step={1}
+                value={volume}
+                onChange={(e) => handleGlobalVolumeChange(e.target.value)}
+                className="global-controls__slider"
+                aria-label="Volume slider"
+                style={{
+                    background: `linear-gradient(to right, #cba6f7 ${volume}%, #45475a ${volume}%)`
+                }}
+            />
+        </div>
+    )
+}
+
+export default GlobalAudioControls;
