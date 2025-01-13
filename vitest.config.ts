9fc3b80ab1643b12f3bcfa01357342e905ee0f78
Alex Glancevs <alex2531458@gmail.com>
2025-01-13 23:57:48 +0200

    chore: setup testing environment (#11)

---
+++ vitest.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig, ViteUserConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+    plugins: [react()] as ViteUserConfig["plugins"],
+    test: {
+        environment: 'jsdom',
+        globals: true,
+        setupFiles: 'test/setup.ts',
+    }
+});
