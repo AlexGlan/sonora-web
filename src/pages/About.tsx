7a13dad75b80971b5d8c816b39a6a0176828ba4c
Alex Glancevs <alex2531458@gmail.com>
2025-01-16 20:35:09 +0200

    feat: add about page component (#14)

---
+++ src/pages/About.tsx
@@ -0,0 +1,50 @@
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import NavBar from "../components/NavBar";
+import audioData from '../store/audioData.json';
+
+const About = () => {
+    return (
+        <>
+            <NavBar />
+            <main className="about">
+                <div className="about__description">
+                    <p className="about__text">
+                        This project provides an easy way to create and enjoy a peaceful environment with background noise and ambient
+                        sounds, all from a single browser tab. It's designed to help you focus, relax, sleep better, or enhance music,
+                        allowing you to mix and adjust sounds to fit your mood and needs.
+                    </p>
+                    <p className="about__text">
+                        I created this project because I enjoy working with white/brown noise and calm ambient sounds in the background
+                        more than music. It helps me to stay focused and productive, and I’ve found it to be an important part of my workflow.
+                        So my goal was to make this experience as simple, customizable, and accessible as possible—for myself and for anyone
+                        else who might find it interesting or helpful 🍀
+                    </p>
+                </div>
+                <div className="about__links">
+                    <p className="about__github">
+                        Source code: <a href="https://github.com/AlexGlan/white-noise-app" target="_blank">Github <FontAwesomeIcon icon={faGithub} /></a>
+                    </p>
+                    <p className="about__credits-text">
+                        ✨ Special thanks to the following people for generously sharing their recordings for free:
+                    </p>
+                    <ul className="about__credits-list" aria-label="Audio Credits">
+                        {
+                            audioData.map(track => {
+                                return (
+                                    <li key={`attr_${track.id}`}>
+                                        <a href={track.originalAudioLink} target="_blank">
+                                            {track.originalName} by {track.authorName}
+                                        </a>
+                                    </li>
+                                );
+                            })
+                        }
+                    </ul>
+                </div>
+            </main>
+        </>
+    )
+}
+
+export default About;
