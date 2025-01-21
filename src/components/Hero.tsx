e19ac8c92fa87284ac4f21575feaac36347b481e
Alex Glancevs <alex2531458@gmail.com>
2025-01-21 14:32:00 +0200

    feat: add particles to the hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,14 +1,19 @@
-import { getImageUrl } from "../utils/imageUtils";
+import ParticleComponent from "./ParticleComponent";
 
 const Hero = () => {
     return (
-        <section className="hero" style={{backgroundImage: `url(${getImageUrl('hero.jpg')})`}}>
-            <h1 className="hero__header">
-                White Noise & Ambient Sound Tool
-            </h1>
-            <p className="hero__subhead">
-                Explore calming soundscapes to unwind, focus, or sleep peacefully.
-            </p>
+        <section className="hero">
+            <div className="hero__particle-container" aria-hidden="true">
+                <ParticleComponent />
+            </div>
+            <div className="hero__content">
+                <h1 className="hero__header">
+                    White Noise & Ambient Sound Tool
+                </h1>
+                <p className="hero__subhead">
+                    Explore calming soundscapes to unwind, focus, or sleep peacefully.
+                </p>
+            </div>
         </section>
     )
 }
