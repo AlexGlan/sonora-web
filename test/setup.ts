cf6a63e18adc7796639cc03ff527d15d978e2470
Alex Glancevs <alex2531458@gmail.com>
2025-01-21 15:08:19 +0200

    test: suppress canvas context warnings in test environment

--- test/setup.ts
+++ test/setup.ts
@@ -1,1 +1,10 @@
 import '@testing-library/jest-dom/vitest';
+
+beforeAll(() => {
+    // Mock canvas context to suppress warnings in test environment
+    HTMLCanvasElement.prototype.getContext = vi.fn();
+});
+
+afterAll(() => {
+    vi.resetAllMocks();
+});
