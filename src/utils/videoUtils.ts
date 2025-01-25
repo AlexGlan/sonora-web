61a53c1ca60ec129a266191e0e144eb49e9cc3e9
Alex Glancevs <alex2531458@gmail.com>
2025-01-26 00:27:24 +0200

    feat: improve parameter matching to support more link variations

--- src/utils/videoUtils.ts
+++ src/utils/videoUtils.ts
@@ -3,7 +3,7 @@ export const validateYoutubeLink = (url: string): boolean => {
     const www = "(www\\.)?"; // Matches optional www.
     const videoPath = "youtube\\.com\\/(watch\\?v=[a-zA-Z0-9]{11}|live\\/[a-zA-Z0-9]{11})";
     const shortUrl = "youtu\\.be\\/[a-zA-Z0-9]{11}"; // Matches shortened YouTube URLs
-    const queryParams = "(\\?[^ ]*)?"; // Matches optional query parameters
+    const queryParams = "([?&][a-zA-Z0-9]+.*)?"; // Matches optional query parameters
 
     const pattern = new RegExp(`^(${protocol})(${www})(${videoPath}|${shortUrl})${queryParams}$`);
     return url != null && pattern.test(url);
