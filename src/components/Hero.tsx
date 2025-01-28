5f4ba206caa988fb64a9196f0a3f14654fdcee15
Alex Glancevs <alex2531458@gmail.com>
2025-01-28 16:09:05 +0200

    feat: add graphic background to hero section (#28)

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,3 +1,4 @@
+import { getImageUrl } from "../utils/imageUtils";
 import ParticleComponent from "./ParticleComponent";
 
 const Hero = () => {
@@ -6,6 +7,11 @@ const Hero = () => {
             <div className="hero__particle-container" aria-hidden="true">
                 <ParticleComponent />
             </div>
+            <div
+                className="hero__graphic-bg"
+                aria-hidden="true"
+                style={{backgroundImage: `url(${getImageUrl('hero-graphic-bg.png')})`}}
+            />
             <div className="hero__content">
                 <h1 className="hero__header">
                     White Noise & Ambient Sound Tool
