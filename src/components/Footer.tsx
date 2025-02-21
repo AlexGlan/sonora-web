2e1555a11749f86c5011abd858f5caef9473d90f
Alex Glancevs <alex2531458@gmail.com>
2025-02-21 23:02:45 +0200

    feat: create footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,19 @@
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="footer__author">
+                <span className="footer__author-text">
+                    Made by
+                    {' '}
+                    <a className="footer__author-link" href="https://github.com/AlexGlan" target="_blank">
+                        alexglan
+                    </a>
+                    {' '}
+                    with 💜
+                </span>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
