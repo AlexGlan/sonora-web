8541b121c5140da17799ab5891c238fa89d22572
Alex Glancevs <alex2531458@gmail.com>
2025-03-14 14:07:47 +0200

    refactor: change base project url

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,7 @@ import routesConfig from './routes/routesConfig';
 import { setupStore } from './store/store';
 import { Provider } from 'react-redux';
 import './styles/main.scss';
-
-const router = createBrowserRouter(routesConfig, {basename: '/white-noise-app/'});
+const router = createBrowserRouter(routesConfig, { basename: '/sonora-web/' });
 const store = setupStore();
 
 createRoot(document.getElementById('root')!).render(
