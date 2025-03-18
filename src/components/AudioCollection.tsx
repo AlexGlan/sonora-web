a560b7849641a72643c6ed3500926400992c3da2
Alex Glancevs <alex2531458@gmail.com>
2025-03-18 22:18:37 +0200

    feat: add functionality for fetching additional audio data

--- src/components/AudioCollection.tsx
+++ src/components/AudioCollection.tsx
@@ -1,8 +1,17 @@
-import { useAppSelector } from "../app/hooks";
+import { useAppDispatch, useAppSelector } from "../app/hooks";
+import { fetchAudioTracks } from "../store/audioSlice.js";
 import AudioTrack from "./AudioTrack";
+import Spinner from "./Spinner.js";
 
 const AudioCollection = () => {
     const tracks = useAppSelector(state => state.audio.tracks.allIds);
+    const status = useAppSelector(state => state.audio.status);
+    const error = useAppSelector(state => state.audio.error);
+    const dispatch = useAppDispatch();
+
+    const handleClick = (): void => {
+        dispatch(fetchAudioTracks());
+    }
 
     return (
         <section className="audio">
@@ -17,6 +26,17 @@ const AudioCollection = () => {
                     })
                 }
             </ul>
+            <div className="audio__load">
+                {status === 'pending'
+                    ? <Spinner />
+                    : <button onClick={handleClick} className="audio__load-btn">Load more audio</button>
+                }
+                {error && <strong className="error-message">{error}</strong>}
+                <span className="audio__load-disclaimer">
+                    *This is a hobby project on free infrastructure, so expect cold starts and some waiting time <span>🐢</span>
+                </span>
+            </div>
+           
         </section>
     )
 }
