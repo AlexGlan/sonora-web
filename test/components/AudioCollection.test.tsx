ba0912c5af09ca7d4a147c148b3e888001b25cc0
Alex Glancevs <alex2531458@gmail.com>
2025-03-25 21:43:34 +0200

    test: add tests for audio data fetching

--- test/components/AudioCollection.test.tsx
+++ test/components/AudioCollection.test.tsx
@@ -1,11 +1,16 @@
 import { screen, within } from '@testing-library/react';
 import { renderWithProviders } from "../test-utils";
+import audioData from '../mocks/audioData.mock.json';
+import { server } from '../mocks/node.ts';
+import { http, HttpResponse } from 'msw';
 
 describe('AudioCollection', () => {
     it('Should render a list of audio tracks', () => {
         renderWithProviders();
+
         const audioList = screen.getByRole('list', {name: /audio/i});
         const audioTracks = within(audioList).getAllByRole('listitem');
+
         audioTracks.forEach(track => {
             expect(within(track).getByRole('img')).toBeInTheDocument();
             expect(within(track).getByText(/[a-z].*/i))
@@ -13,5 +18,34 @@ describe('AudioCollection', () => {
             expect(within(track).getByRole('slider', {name: /volume/i})).toBeInTheDocument();
             expect(within(track).getByRole('audio')).toBeInTheDocument();
         });
+    });
+
+    it('Should load and render additional audio tracks when the data fetching button is clicked', async () => {
+        const { user } = renderWithProviders();
+
+        await user.click(screen.getByRole('button', { name: /load/i }));
+
+        const audioList = screen.getByRole('list', {name: /audio/i});
+        const audioTracks = within(audioList).getAllByRole('listitem');
+
+        expect(audioTracks.length).toBe(audioData.data.length);
+        audioTracks.forEach((track, i) => {
+            const name = within(track).getByText(/[a-z].*/i);
+            expect(name).toHaveTextContent(audioData.data[i].name);
+            const audio = within(track).getByRole('audio') as HTMLAudioElement;
+            expect(audio.src).toBe(audioData.data[i].audioSrc);
+        });
+    });
+
+    it('Should display an error message when request fails', async () => {
+        const { user } = renderWithProviders();
+        // Mock an error response
+        server.use(http.get('https://sonora-api-2oow.onrender.com/api/audio', () => {
+            return HttpResponse.json({ error: 'Server error' }, { status: 500 });
+        }));
+
+        await user.click(screen.getByRole('button', { name: /load/i }));
+
+        expect(screen.getByText(/unable|error/i)).toBeInTheDocument();
     });
 });
