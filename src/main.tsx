7c5b3205dbafc2ad913f16909256f57d33d514ee
Alex Glancevs <alex2531458@gmail.com>
2025-04-01 16:44:17 +0300

    feat: add preloaded state from localStorage in store setup

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createHashRouter, RouterProvider } from 'react-router-dom';
 import routesConfig from './routes/routesConfig';
-import { setupStore } from './store/store';
 import { Provider } from 'react-redux';
+import store from './store/store';
 import './styles/main.scss';
 
 const router = createHashRouter(routesConfig);
-const store = setupStore();
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
