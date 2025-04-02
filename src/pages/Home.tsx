c1232cd46dcaf186339ca3143a98422c56b43f08
Alex Glancevs <alex2531458@gmail.com>
2025-04-02 16:45:28 +0300

    refactor: remove redundant layout elements from pages

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,16 @@
 import AudioCollection from "../components/AudioCollection";
-import Footer from "../components/Footer";
 import GlobalAudioControls from "../components/GlobalAudioControls";
 import Hero from "../components/Hero";
-import NavBar from "../components/NavBar";
 import YoutubeSection from "../components/YoutubeSection";
 
 const Home = () => {
     return(
-        <>
-            <NavBar />
-            <main>
-                <Hero />
-                <GlobalAudioControls />
-                <AudioCollection />
-                <YoutubeSection />
-            </main>
-            <Footer />
-        </>
+        <main>
+            <Hero />
+            <GlobalAudioControls />
+            <AudioCollection />
+            <YoutubeSection />
+        </main>
     )
 }
 
