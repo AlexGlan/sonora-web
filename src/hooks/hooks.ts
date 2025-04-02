57cdf1068e573ce7d6c44159265f5665b64c1598
Alex Glancevs <alex2531458@gmail.com>
2025-04-02 16:21:43 +0300

    feat: add custom hook to auto-scroll to top on route change

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -1,5 +1,15 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, RootState } from '../store/store';
+import { useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
 export const useAppSelector = useSelector.withTypes<RootState>();
+
+export const useScrollToTop = (): void => {
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [pathname]);
+}
